b6d46c50000b18f0b568d3ae9faf391193c49c31
Jesus Sayar Celestino <jesus.sayar@softwhisper.es>
2012-05-30 22:53:30 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#Product.create(:name => 'Zapatos', :description => 'Pareja de zapatos', :price => 20)
-#Product.create(:name => 'Camisa', :description => 'Camisa blanco', :price => 30)
-#Product.create(:name => 'Pantalon', :description => 'Pantalones marrones', :price => 40)
-#Product.create(:name => 'Jersey', :description => 'Jersey de lana', :price => 50)
-#Product.create(:name => 'Chaqueta', :description => 'Chaqueta negra', :price => 100)
-#Product.create(:name => 'Calcetines', :description => 'Calcetines blancos', :price => 3)
+Product.create(:name => 'Zapatos', :description => 'Pareja de zapatos', :price => 20)
+Product.create(:name => 'Camisa', :description => 'Camisa blanco', :price => 30)
+Product.create(:name => 'Pantalon', :description => 'Pantalones marrones', :price => 40)
+Product.create(:name => 'Jersey', :description => 'Jersey de lana', :price => 50)
+Product.create(:name => 'Chaqueta', :description => 'Chaqueta negra', :price => 100)
+Product.create(:name => 'Calcetines', :description => 'Calcetines blancos', :price => 3)
 
 User.create(:email => "foobar@shop.es", :password=> "abcd_1234")
 User.create(:email => "admin@shop.es", :password=> "abcd_1234", :admin => true)
